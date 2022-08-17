43430a2ee70bcbe947c4d441377da987d6478ed8
apennamen <adrien.pennamen@gmail.com>
2022-08-17 12:11:37 +0200

    chore: change logger to use default

--- __init__.py
+++ __init__.py
@@ -1,8 +1,5 @@
 import subprocess
 from mycroft import MycroftSkill, intent_handler
-from mycroft.util.log import getLogger
-
-LOGGER = getLogger(__name__)
 
 
 class ScriptLauncher(MycroftSkill):
@@ -21,13 +18,13 @@ class ScriptLauncher(MycroftSkill):
     def handle_launcher_script(self, message):
         # Extract script alias from intent
         script_alias = message.data.get('script_alias')
-        LOGGER.debug("Script alias received: %s" % script_alias)
+        self.log.debug("Script alias received: %s" % script_alias)
         if script_alias is None:
             return self.speak_dialog('no.script.found')
 
         # Find script to execute
         script_to_exec = self.scripts_registry.get(script_alias)
-        LOGGER.debug("Script to execute: %s" % script_alias)
+        self.log.debug("Script to execute: %s" % script_alias)
         if script_to_exec is None:
             return self.speak_dialog('no.script.found')
 
@@ -37,7 +34,7 @@ class ScriptLauncher(MycroftSkill):
         process.wait()
 
         # Handle script return code
-        LOGGER.debug("Script return code: %s" % process.returncode)
+        self.log.debug("Script return code: %s" % process.returncode)
         if process.returncode != 0:
             return self.speak_dialog('script.error')
 
