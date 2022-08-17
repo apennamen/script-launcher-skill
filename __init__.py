7f14d5f043bd55aa31c060debdbf2687365dfc29
apennamen <adrien.pennamen@gmail.com>
2022-08-17 09:45:49 +0200

    feat(script): launch script

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
-from mycroft import MycroftSkill, intent_file_handler
+import subprocess
+from mycroft import MycroftSkill, intent_handler
+from mycroft.util.log import getLogger
 
+LOGGER =  getLogger(__name__)
 
 class ScriptLauncher(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('launcher.script.intent')
+    @intent_handler('launcher.script.intent')
     def handle_launcher_script(self, message):
+        process = subprocess.Popen("/home/pi/picroft-scripts/connect-speaker.sh", 
+                                   shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        LOGGER.debug(process.returncode)
         self.speak_dialog('launcher.script')
 
 
